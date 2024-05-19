f5c9ef468f5e3a461e71b3ac5b970d6d8180a0f7
LokeshDandumahanti <146311601+LokeshDandumahanti@users.noreply.github.com>
2024-05-19 17:18:02 +0530

    Update Banglore_Prices.py

--- Banglore_Prices.py
+++ Banglore_Prices.py
@@ -1,7 +1,9 @@
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import matplotlib as plt
 
 # Load the model and columns
 lr_clf = joblib.load("C:/Users/vijay/OneDrive/Desktop/Banglore Housing Project/banglore_home_prices_model.pkl")
